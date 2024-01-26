cb1bb4b062c2293c150997325f23c96907f290a6
brycenaddison <b@brycenaddison.com>
2024-01-26 12:03:49 -0500

    feat: Make scoreboard components toggleable

--- src/app/match/[matchId]/components/PlayerScoreboard.tsx
+++ src/app/match/[matchId]/components/PlayerScoreboard.tsx
@@ -9,12 +9,34 @@ import { cn } from '@/lib/utils';
 export type PlayerScoreboardProps = {
   participant: Riot.MatchV5.Participant;
   size?: 'sm' | 'md' | 'lg';
+  show?: {
+    role?: boolean;
+    level?: boolean;
+    spells?: boolean;
+    runes?: boolean;
+    items?: boolean;
+    name?: boolean;
+    kda?: boolean;
+  };
 };
 
 export const PlayerScoreboard = async ({
   participant,
   size = 'md',
+  show: options = {},
 }: PlayerScoreboardProps): Promise<JSX.Element> => {
+  const defaults = {
+    role: true,
+    level: true,
+    spells: true,
+    runes: true,
+    items: true,
+    name: true,
+    kda: true,
+  };
+
+  const show = Object.assign({}, defaults, options);
+
   const summonerName = await getSummonerName(participant.puuid);
   const champId = participant.championId;
 
@@ -28,15 +50,17 @@ export const PlayerScoreboard = async ({
 
   return (
     <div className='flex items-center'>
-      <Role role={participant.teamPosition} size={size} />
-      {size === 'sm' && (
+      {show.role && <Role role={participant.teamPosition} size={size} />}
+
+      {show.level && size === 'sm' && (
         <div className='mb-0.5 ml-1 font-semibold'>
           {participant.champLevel}
         </div>
       )}
+
       <div className='relative ml-2'>
         <ChampIcon champId={champId} size={size} />
-        {size !== 'sm' && (
+        {size !== 'sm' && show.level && (
           <div
             className={cn(
               'absolute bottom-0 right-0 bg-black/50 text-center h-6 w-6 rounded-tl-lg rounded-br-lg',
@@ -50,95 +74,110 @@ export const PlayerScoreboard = async ({
           </div>
         )}
       </div>
-      <div
-        className={cn('flex flex-col shrink-0', {
-          'ml-1 gap-0.5': size === 'sm',
-          'ml-1.5 gap-0.5': size === 'md',
-          'ml-2 gap-1': size === 'lg',
-        })}
-      >
-        <SummonerSpell spellId={participant.summoner1Id} size={size} />
-        <SummonerSpell spellId={participant.summoner2Id} size={size} />
-      </div>
-      <div
-        className={cn('flex flex-col items-center justify-between shrink-0', {
-          'ml-0.5 gap-0.5': size === 'sm',
-          'ml-[3px] gap-0.5': size === 'md',
-          'ml-1 gap-1': size === 'lg',
-        })}
-      >
-        <Rune runeData={participant.perks} type='keystone' size={size} />
-        <Rune
-          runeData={participant.perks}
-          type='secondary'
-          size={size}
-          className={cn({
-            'p-1': size === 'lg',
-            'p-[3px]': size === 'md',
-            'p-0.5': size === 'sm',
-          })}
-        />
-      </div>
-      <div
-        className={cn('font-semibold', {
-          'text-2xl mb-1 ml-3': size === 'lg',
-          'text-xl mb-[3px] ml-[9px]': size === 'md',
-          'text-lg mb-0.5 ml-1.5': size === 'sm',
-        })}
-      >
-        {summonerName}
-      </div>
-      <div
-        className={cn(
-          'flex flex-col text-gray-200 items-center whitespace-nowrap',
-          {
-            'ml-2 text-xl': size === 'lg',
-            'ml-[9px] text-md]': size === 'md',
-            'ml-1.5 text-xs': size === 'sm',
-          },
-        )}
-      >
+
+      {show.spells && (
         <div
-          className={cn('flex mt-auto font-semibold', {
+          className={cn('flex flex-col shrink-0', {
+            'ml-1 gap-0.5': size === 'sm',
+            'ml-1.5 gap-0.5': size === 'md',
+            'ml-2 gap-1': size === 'lg',
+          })}
+        >
+          <SummonerSpell spellId={participant.summoner1Id} size={size} />
+          <SummonerSpell spellId={participant.summoner2Id} size={size} />
+        </div>
+      )}
+
+      {show.runes && (
+        <div
+          className={cn('flex flex-col items-center justify-between shrink-0', {
+            'ml-0.5 gap-0.5': size === 'sm',
+            'ml-[3px] gap-0.5': size === 'md',
+            'ml-1 gap-1': size === 'lg',
+          })}
+        >
+          <Rune runeData={participant.perks} type='keystone' size={size} />
+          <Rune
+            runeData={participant.perks}
+            type='secondary'
+            size={size}
+            className={cn({
+              'p-1': size === 'lg',
+              'p-[3px]': size === 'md',
+              'p-0.5': size === 'sm',
+            })}
+          />
+        </div>
+      )}
+
+      {show.name && (
+        <div
+          className={cn('font-semibold', {
+            'text-2xl mb-1 ml-3': size === 'lg',
+            'text-xl mb-[3px] ml-[9px]': size === 'md',
+            'text-lg mb-0.5 ml-1.5': size === 'sm',
+          })}
+        >
+          {summonerName}
+        </div>
+      )}
+
+      {show.kda && (
+        <div
+          className={cn(
+            'flex flex-col text-gray-200 items-center whitespace-nowrap',
+            {
+              'ml-2 text-xl': size === 'lg',
+              'ml-[9px] text-md]': size === 'md',
+              'ml-1.5 text-xs': size === 'sm',
+            },
+          )}
+        >
+          <div
+            className={cn('flex mt-auto font-semibold', {
+              'gap-1': size === 'lg',
+              'gap-[3px]': size === 'md',
+              'gap-0.5': size === 'sm',
+            })}
+          >
+            <span>{participant.kills}</span>
+            <span className='text-gray-500 font-normal'>/</span>
+            <span>{participant.deaths}</span>
+            <span className='text-gray-500 font-normal'>/</span>
+            <span>{participant.assists}</span>
+          </div>
+          <div className='mb-auto'>
+            <span
+              className={cn('font-semibold', {
+                'text-orange-400': kda === 'Perfect' || kda >= 5,
+                'text-blue-400': kda !== 'Perfect' && kda >= 3 && kda < 5,
+                'text-white': kda !== 'Perfect' && kda < 3,
+              })}
+            >
+              {kda === 'Perfect' ? kda : `${kda.toFixed(2)}`}
+            </span>
+            <span className='text-gray-300'> KDA</span>
+          </div>
+        </div>
+      )}
+
+      {show.items && (
+        <div
+          className={cn('flex ml-4 shrink-0', {
             'gap-1': size === 'lg',
             'gap-[3px]': size === 'md',
             'gap-0.5': size === 'sm',
           })}
         >
-          <span>{participant.kills}</span>
-          <span className='text-gray-500 font-normal'>/</span>
-          <span>{participant.deaths}</span>
-          <span className='text-gray-500 font-normal'>/</span>
-          <span>{participant.assists}</span>
+          <Item item={participant.item0} size={size} />
+          <Item item={participant.item1} size={size} />
+          <Item item={participant.item2} size={size} />
+          <Item item={participant.item3} size={size} />
+          <Item item={participant.item4} size={size} />
+          <Item item={participant.item5} size={size} />
+          <Item item={participant.item6} size={size} />
         </div>
-        <div className='mb-auto'>
-          <span
-            className={cn('font-semibold', {
-              'text-orange-400': kda === 'Perfect' || kda >= 5,
-              'text-blue-400': kda !== 'Perfect' && kda >= 3 && kda < 5,
-              'text-white': kda !== 'Perfect' && kda < 3,
-            })}
-          >
-            {kda === 'Perfect' ? kda : `${kda.toFixed(2)}`}
-          </span>
-          <span className='text-gray-300'> KDA</span>
-        </div>
-      </div>
-      <div
-        className={cn('flex ml-4 shrink-0', {
-          'gap-1': size === 'lg',
-          'gap-[3px]': size === 'md',
-          'gap-0.5': size === 'sm',
-        })}
-      >
-        <Item item={participant.item0} size={size} />
-        <Item item={participant.item1} size={size} />
-        <Item item={participant.item2} size={size} />
-        <Item item={participant.item3} size={size} />
-        <Item item={participant.item4} size={size} />
-        <Item item={participant.item5} size={size} />
-        <Item item={participant.item6} size={size} />
-      </div>
+      )}
     </div>
   );
 };
