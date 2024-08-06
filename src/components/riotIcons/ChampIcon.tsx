ac1889383e70c56be8003064a8a0280516282fd0
Brycen Addison <b@brycenaddison.com>
2024-08-06 16:26:23 +1200

    feat: added responsive resizing option to ChampIcon

--- src/components/riotIcons/ChampIcon.tsx
+++ src/components/riotIcons/ChampIcon.tsx
@@ -5,14 +5,18 @@ import { cn } from '@/lib/utils';
 const BASE_URL =
   'https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/';
 
+/** Props for {@link ChampIcon}. */
 export type ChampIconProps = {
+  /** The numeric id of the champion. */
   champId?: number;
+  /** The size of the icon (16, 32, 48, or 64px). */
   size?: 'sm' | 'md' | 'lg' | 'xs';
 };
 
+/** A styled and wrapped icon representing a champion. */
 export const ChampIcon = ({
   champId = -1,
-  size = 'lg',
+  size,
 }: ChampIconProps): JSX.Element => (
   <div
     className={cn('overflow-hidden rounded-lg shadow-tile', {
@@ -20,6 +24,7 @@ export const ChampIcon = ({
       'w-8 h-8': size === 'sm',
       'w-12 h-12': size === 'md',
       'w-16 h-16': size === 'lg',
+      'w-8 h-8 lg:w-12 lg:h-12 2xl:w-16 2xl:h-16': size === undefined,
     })}
   >
     <Image
